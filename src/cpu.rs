013f407f17b8a8442dd5ccb7cd2fa9e363e987b2
nakamo326 <nakamo326@gmail.com>
2024-07-03 23:30:58 +0900

    feat: Add CPU module with fetch, decode, and emulate_cycle functions

---
+++ src/cpu.rs
@@ -0,0 +1,38 @@
+mod registers;
+
+use super::peripherals::Peripherals;
+use registers::Registers;
+
+#[derive(Default)]
+struct Ctx {
+    opcode: u8,
+    cb: bool,
+}
+
+pub struct Cpu {
+    regs: Registers,
+    ctx: Ctx,
+}
+
+impl Cpu {
+    pub fn fetch(&mut self, bus: &Peripherals) {
+        self.ctx.opcode = bus.read(self.regs.pc);
+        self.regs.pc = self.regs.pc.wrapping_add(1);
+        self.ctx.cb = false;
+    }
+
+    pub fn decode(&mut self, bus: &mut Peripherals) {
+        match self.ctx.opcode {
+            0x00 => self.nop(bus),
+            _ => unimplemented!("opcode: {:#02x}", self.ctx.opcode),
+        }
+    }
+
+    fn nop(&mut self, bus: &Peripherals) {
+        self.fetch(bus);
+    }
+
+    pub fn emulate_cycle(&mut self, bus: &mut Peripherals) {
+        self.decode(bus);
+    }
+}
