1f4914941d3cc819a5cc985294474091b729d842
Yasufumi Nakamoto <nakamo326@gmail.com>
2024-08-11 16:57:49 +0900

    add direct16 operation

--- src/cpu/operand.rs
+++ src/cpu/operand.rs
@@ -270,3 +270,33 @@ impl IO8<Direct8> for Cpu {
         });
     }
 }
+
+impl IO16<Direct16> for Cpu {
+    fn read16(&mut self, bus: &Peripherals, src: Direct16) -> Option<u16> {
+        unreachable!()
+    }
+
+    fn write16(&mut self, bus: &mut Peripherals, dst: Direct16, val: u16) -> Option<()> {
+        step!(None, {
+            0: if let Some(lo) = self.read8(bus,Imm8) {
+                VAL8.store(lo, Relaxed);
+                go!(1);
+            },
+            1: if let Some(hi) = self.read8(bus, Imm8) {
+                    VAL16.store(u16::from_le_bytes([VAL8.load(Relaxed), hi]), Relaxed);
+                    go!(2);
+            },
+            2: {
+                bus.write(VAL16.load(Relaxed), val as u8);
+                go!(3);
+                return None;
+            },
+            3: {
+                bus.write(VAL16.load(Relaxed).wrapping_add(1), (val >> 8) as u8);
+                go!(4);
+                return None;
+            },
+            4: return Some(go!(0)),
+        });
+    }
+}
