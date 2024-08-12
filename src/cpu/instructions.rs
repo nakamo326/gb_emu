7462f72fd121d77b9243c723bebc21f659436107
Yasufumi Nakamoto <nakamo326@gmail.com>
2024-08-12 09:18:00 +0900

    add ld instruction

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -1,3 +1,10 @@
+use super::{
+    operand::{IO16, IO8},
+    peripherals::Peripherals,
+    Cpu,
+};
+use std::sync::atomic::{AtomicU16, AtomicU8, Ordering::Relaxed};
+
 macro_rules! step {
     ($d:expr, {$($c:tt : $e:expr,)*}) => {
         static STEP: AtomicU8 = AtomicU8::new(0);
@@ -16,3 +23,43 @@ macro_rules! go {
     };
 }
 pub(crate) use go;
+
+impl Cpu {
+    pub fn ld<D: Copy, S: Copy>(&mut self, bus: &mut Peripherals, dst: D, src: S)
+    where
+        Self: IO8<D> + IO8<S>,
+    {
+        step!((), {
+            0: if let Some(val) = self.read8(bus, src) {
+                VAL8.store(val, Relaxed);
+                go!(1);
+            },
+            1: if self.write8(bus, dst, VAL8.load(Relaxed)).is_some() {
+                go!(2);
+            },
+            2: {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn ld16<D: Copy, S: Copy>(&mut self, bus: &mut Peripherals, dst: D, src: S)
+    where
+        Self: IO16<D> + IO16<S>,
+    {
+        step!((), {
+            0: if let Some(v) = self.read16(bus, src) {
+                VAL16.store(v, Relaxed);
+                go!(1);
+            },
+            1: if self.write16(bus, dst, VAL16.load(Relaxed)).is_some() {
+                go!(2);
+            },
+            2: {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+}
