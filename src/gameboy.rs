f14ac79d151cd8308eef58f68757e5b4c0b08f13
Ynakamot <nakamo326@gmail.com>
2025-06-22 13:02:25 +0900

    refactor: refcellを削除

--- src/gameboy.rs
+++ src/gameboy.rs
@@ -1,14 +1,8 @@
-use std::cell::RefCell;
-use std::rc::Rc;
 use std::time;
 
-use crate::bootrom::Bootrom;
 use crate::cpu::Cpu;
-use crate::hram::HRam;
 use crate::lcd::Lcd;
 use crate::mmu::Mmu;
-use crate::ppu::Ppu;
-use crate::wram::WRam;
 
 pub const CPU_CLOCK_HZ: u128 = 4_194_304;
 pub const M_CYCLE_CLOCK: u128 = 4;
@@ -16,34 +10,15 @@ const M_CYCLE_NANOS: u128 = M_CYCLE_CLOCK * 1_000_000_000 / CPU_CLOCK_HZ;
 
 pub struct GameBoy {
     cpu: Cpu,
-    bootrom: Rc<RefCell<Bootrom>>,
-    wram: Rc<RefCell<WRam>>,
-    hram: Rc<RefCell<HRam>>,
-    ppu: Rc<RefCell<Ppu>>,
     mmu: Mmu,
     lcd: Lcd,
 }
 
 impl GameBoy {
     pub fn new() -> Self {
-        let bootrom = Rc::new(RefCell::new(Bootrom::new("dmg_bootrom.bin")));
-        let wram = Rc::new(RefCell::new(WRam::new()));
-        let hram = Rc::new(RefCell::new(HRam::new()));
-        let ppu = Rc::new(RefCell::new(Ppu::new()));
-        let mmu = Mmu {
-            bootrom: bootrom.clone(),
-            wram: wram.clone(),
-            hram: hram.clone(),
-            ppu: ppu.clone(),
-        };
-
         Self {
             cpu: Cpu::new(),
-            bootrom,
-            wram,
-            hram,
-            ppu,
-            mmu,
+            mmu: Mmu::new(),
             lcd: Lcd::new(),
         }
     }
@@ -55,8 +30,8 @@ impl GameBoy {
             let e = time.elapsed().as_nanos();
             for _ in 0..(e - elapsed) / M_CYCLE_NANOS {
                 self.cpu.emulate_cycle(&mut self.mmu);
-                if self.mmu.ppu.borrow_mut().emulate_cycle() {
-                    self.lcd.draw(self.mmu.ppu.borrow().pixel_buffer());
+                if self.mmu.ppu.emulate_cycle() {
+                    self.lcd.draw(self.mmu.ppu.pixel_buffer());
                 }
                 elapsed += M_CYCLE_NANOS;
             }
--- src/mmu.rs
+++ src/mmu.rs
@@ -1,45 +1,56 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-
 use crate::bootrom::Bootrom;
 use crate::hram::HRam;
 use crate::ppu::Ppu;
 use crate::wram::WRam;
 
 pub struct Mmu {
-    pub bootrom: Rc<RefCell<Bootrom>>,
-    pub wram: Rc<RefCell<WRam>>,
-    pub hram: Rc<RefCell<HRam>>,
-    pub ppu: Rc<RefCell<Ppu>>,
+    pub bootrom: Bootrom,
+    pub wram: WRam,
+    pub hram: HRam,
+    pub ppu: Ppu,
 }
 
 impl Mmu {
+    pub fn new() -> Self {
+        let bootrom = Bootrom::new("dmg_bootrom.bin");
+        let wram = WRam::new();
+        let hram = HRam::new();
+        let ppu = Ppu::new();
+
+        Self {
+            bootrom,
+            wram,
+            hram,
+            ppu,
+        }
+    }
+
     pub fn read(&self, addr: u16) -> u8 {
         match addr {
             0x0000..=0x00FF => {
-                if self.bootrom.borrow().is_active() {
-                    self.bootrom.borrow().read(addr)
+                if self.bootrom.is_active() {
+                    self.bootrom.read(addr)
                 } else {
                     0xFF
                 }
             }
-            0x8000..=0x9FFF => self.ppu.borrow().read(addr),
-            0xFE00..=0xFE9F => self.ppu.borrow().read(addr),
-            0xFF40..=0xFF4B => self.ppu.borrow().read(addr),
-            0xC000..=0xFDFF => self.wram.borrow().read(addr),
-            0xFF80..=0xFFFE => self.hram.borrow().read(addr),
+            0x8000..=0x9FFF => self.ppu.read(addr),
+            0xFE00..=0xFE9F => self.ppu.read(addr),
+            0xFF40..=0xFF4B => self.ppu.read(addr),
+            0xC000..=0xFDFF => self.wram.read(addr),
+            0xFF80..=0xFFFE => self.hram.read(addr),
             _ => 0xFF,
         }
     }
 
     pub fn write(&mut self, addr: u16, val: u8) {
         match addr {
-            0x8000..=0x9FFF => self.ppu.borrow_mut().write(addr, val),
-            0xFE00..=0xFE9F => self.ppu.borrow_mut().write(addr, val),
-            0xFF40..=0xFF4B => self.ppu.borrow_mut().write(addr, val),
-            0xC000..=0xFDFF => self.wram.borrow_mut().write(addr, val),
-            0xFF50 => self.bootrom.borrow_mut().write(addr, val),
-            0xFF80..=0xFFFE => self.hram.borrow_mut().write(addr, val),
+            0x8000..=0x9FFF => self.ppu.write(addr, val),
+            0xFE00..=0xFE9F => self.ppu.write(addr, val),
+            0xFF40..=0xFF4B => self.ppu.write(addr, val),
+            0xC000..=0xFDFF => self.wram.write(addr, val),
+            0xFF50 => self.bootrom.write(addr, val),
+            0xFF80..=0xFFFE => self.hram.write(addr, val),
             _ => (),
         }
     }
